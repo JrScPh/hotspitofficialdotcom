6ecef419af56d9177d8044f048b764b81fca9dd0
Jared Scott Phillips <jaredscottphillips13@gmail.com>
2026-02-23 19:47:48 -0600

    V0.61

--- js/Icons.js
+++ js/Icons.js
@@ -1,31 +1,33 @@
 // Function to load website icons dynamically
-document.addEventListener("DOMContentLoaded", () => {
-    const icons = [
-        { name: "Instagram", url: "https://www.instagram.com/hotspitofficial", icon: "icons/instagram.png" },
-        { name: "Bandcamp", url: "https://hotspitofficial.bandcamp.com", icon: "icons/bandcamp.png" },
-        { name: "SoundCloud", url: "https://www.soundcloud.com/hotspitofficial", icon: "icons/soundcloud.png" },
-        { name: "YouTube", url: "https://www.youtube.com/@hotspitofficial", icon: "icons/youtube.png" }
-    ];
 
-    const iconsContainer = document.createElement("div");
-    iconsContainer.classList.add("icons-container");
+const icons = [
+    { name: "Instagram", url: "https://www.instagram.com/hotspitofficial", icon: "icons/instagram.png" },
+    { name: "Bandcamp", url: "https://hotspitofficial.bandcamp.com", icon: "icons/bandcamp.png" },
+    { name: "SoundCloud", url: "https://www.soundcloud.com/hotspitofficial", icon: "icons/soundcloud.png" },
+    { name: "YouTube", url: "https://www.youtube.com/@hotspitofficial", icon: "icons/youtube.png" }
+];
 
-    icons.forEach(({ name, url, icon }) => {
-        const link = document.createElement("a");
-        link.href = url;
-        link.target = "_blank";
-        link.rel = "noopener noreferrer";
-        link.classList.add("icon-link");
+const iconsContainer = document.createElement("div");
+iconsContainer.classList.add("icons-container");
 
-        const img = document.createElement("img");
-        img.src = icon;
-        img.alt = `${name} Icon`;
-        img.classList.add("icon-image");
+icons.forEach(({ name, url, icon }) => {
+    const link = document.createElement("a");
+    link.href = url;
+    link.target = "_blank";
+    link.rel = "noopener noreferrer";
+    link.classList.add("icon-link");
 
-        link.appendChild(img);
-        iconsContainer.appendChild(link);
-    });
+    const img = document.createElement("img");
+    img.src = icon;
+    img.alt = `${name} Icon`;
+    img.classList.add("icon-image");
 
-    const emailElement = document.querySelector(".email");
-    emailElement.insertAdjacentElement("afterend", iconsContainer);
+    link.appendChild(img);
+    iconsContainer.appendChild(link);
 });
+
+const emailElement = document.querySelector(".email");
+
+if (emailElement) {
+    emailElement.insertAdjacentElement("afterend", iconsContainer);
+}
